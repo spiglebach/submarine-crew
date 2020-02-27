d7abb33f4bfa81ebd80e50f903fe30c7237f0f90
Szukács Marcell <spiglebach@gmail.com>
2020-02-27 11:13:03 +0100

    initial commit, generated libGDX project

---
+++ desktop/src/szm/orde4c/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package szm.orde4c.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import szm.orde4c.game.SubmarineCrew;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SubmarineCrew(), config);
+	}
+}
