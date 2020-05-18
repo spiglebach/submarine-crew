70945d1e5e2233787445b05f15dbed456c1832ae
Szukács Marcell <spiglebach@gmail.com>
2020-05-18 10:27:47 +0200

    + finalize SaveSlot

--- core/src/szm/orde4c/game/ui/SaveSlot.java
+++ core/src/szm/orde4c/game/ui/SaveSlot.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import szm.orde4c.game.base.BaseActor;
 import szm.orde4c.game.base.BaseGame;
+import szm.orde4c.game.util.Assets;
 import szm.orde4c.game.util.Save;
 
 public class SaveSlot extends BaseActor {
@@ -14,18 +15,18 @@ public class SaveSlot extends BaseActor {
     public SaveSlot(float width, float height, int maximumLevelIndex, Save save, Stage s) {
         super(0, 0, s);
         emptySlot = save == null;
-        loadTexture("saveslot.jpg");
+        loadTexture(Assets.instance.getTexture(Assets.SAVE_SLOT));
         setSize(width, height);
         if (save != null) {
             int completedLevels = save.getCompletedLevels();
             float completionPercent = (float) completedLevels * 100 / (float) maximumLevelIndex;
 
-            Label saveNameLabel = new Label(String.format("%d. Mentés", save.getId()), BaseGame.largeLabelStyle);
+            Label saveNameLabel = new Label(String.format("%d. Mentés", save.getId()), BaseGame.labelStyle);
             saveNameLabel.setFontScale(0.5f);
 
-            Label completionPercentLabel = new Label("Haladás: " + (int) Math.ceil(completionPercent) + "%", BaseGame.largeLabelStyle);
+            Label completionPercentLabel = new Label("Haladás: " + completionPercent + "%", BaseGame.labelStyle);
             completionPercentLabel.setFontScale(0.5f);
-            Label completedLevelsLabel = new Label(String.format("Végigvitt szintek: %d", completedLevels), BaseGame.largeLabelStyle);
+            Label completedLevelsLabel = new Label(String.format("Teljesített szintek: %d", completedLevels), BaseGame.labelStyle);
             completedLevelsLabel.setFontScale(0.5f);
             Table layoutTable = new Table();
             layoutTable.setFillParent(true);
