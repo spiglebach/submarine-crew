c7a01239074e518981d5e7a8879b05275bd1d83b
Szukács Marcell <spiglebach@gmail.com>
2020-05-18 10:32:33 +0200

    + finalize ButtonIndicator

--- core/src/szm/orde4c/game/ui/ButtonIndicator.java
+++ core/src/szm/orde4c/game/ui/ButtonIndicator.java
@@ -1,33 +1,29 @@
 package szm.orde4c.game.ui;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import szm.orde4c.game.base.BaseActor;
-import szm.orde4c.game.util.CustomActions;
+import szm.orde4c.game.util.Assets;
 
 import java.util.ArrayList;
 
 public class ButtonIndicator extends BaseActor {
-    public static final int CONTROLLER_FACE_BUTTON_BASE = 0;
-    public static final int CONTROLLER_FACE_BUTTON_NORTH = 1;
-    public static final int CONTROLLER_FACE_BUTTON_EAST = 2;
-    public static final int CONTROLLER_FACE_BUTTON_SOUTH = 3;
-    public static final int CONTROLLER_FACE_BUTTON_WEST = 4;
-    public static final int CONTROLLER_BUTTON_BACK = 5;
-    public static final int CONTROLLER_BUTTON_START = 6;
+    public static final int CONTROLLER_FACE_BLANK = 0;
+    public static final int CONTROLLER_FACE_EAST = 1;
+    public static final int CONTROLLER_FACE_NORTH = 2;
+    public static final int CONTROLLER_FACE_WEST = 3;
+    public static final int CONTROLLER_FACE_SOUTH = 4;
+    public static final int CONTROLLER_BACK = 5;
+    public static final int CONTROLLER_START = 6;
+    public static final int CONTROLLER_JOYSTICK_X = 7;
+    public static final int CONTROLLER_JOYSTICK_Y = 8;
 
-    public static final int KEYBOARD_BUTTON_ENTER = 7;
-    public static final int KEYBOARD_BUTTON_BACKSPACE = 8;
-    public static final int KEYBOARD_BUTTON_W = 9;
-    public static final int KEYBOARD_BUTTON_S = 10;
-    public static final int KEYBOARD_BUTTON_A = 11;
-    public static final int KEYBOARD_BUTTON_D = 12;
-    public static final int KEYBOARD_BUTTON_E = 13;
-    public static final int KEYBOARD_BUTTON_F = 14;
-    public static final int KEYBOARD_BUTTON_DELETE = 15;
-    public static final int PADLOCK = 16;
+    public static final int KEYBOARD_E = 9;
+    public static final int KEYBOARD_F = 10;
+    public static final int KEYBOARD_DELETE = 11;
+    public static final int KEYBOARD_AD = 12;
+    public static final int KEYBOARD_WS = 13;
+    public static final int PADLOCK = 14;
 
     private ArrayList<Animation> animations;
     private float size;
@@ -36,42 +32,39 @@ public class ButtonIndicator extends BaseActor {
         super(0, 0, s);
         this.size = size;
 
-        Animation faceButtonBase = loadTexture("button/button_display_foreground.png");
-        Animation faceButtonhighlightNorth = loadTexture("button/highlight_north.png");
-        Animation faceButtonHighlightEast = loadTexture("button/controller_buttons_east_red.png");
-        Animation faceButtonHighlightSouth = loadTexture("button/highlight_south.png");
-        Animation faceButtonHighlightWest = loadTexture("button/highlight_west.png");
-        Animation controllerButtonBack = loadTexture("button/controller_button_back.png");
-        Animation controllerButtonStart = loadTexture("button/controller_button_start.png");
-        Animation keyBoardEnterButton = loadTexture("button/enter_button.png");
-        Animation keyboardBackspaceButton = loadTexture("button/backspace.png");
-        Animation keyboardWButton = loadTexture("button/keyboard_button_w.png");
-        Animation keyboardSButton = loadTexture("button/keyboard_button_s.png");
-        Animation keyboardAButton = loadTexture("button/keyboard_button_a.png");
-        Animation keyboardDButton = loadTexture("button/keyboard_button_d.png");
-        Animation keyboardEButton = loadTexture("button/keyboard_button_e.png");
-        Animation keyboardFButton = loadTexture("button/keyboard_button_f.png");
-        Animation keyboardDeleteButton = loadTexture("button/keyboard_button_delete.png");
-        Animation padlock = loadTexture("button/padlock.png");
+        Animation faceBlank = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_FACE_BLANK));
+        Animation faceEast = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_FACE_EAST));
+        Animation faceNorth = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_FACE_NORTH));
+        Animation faceWest = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_FACE_WEST));
+        Animation faceSouth = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_FACE_SOUTH));
+        Animation controllerBack = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_BACK));
+        Animation controllerStart = loadTexture(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_START));
+        Animation joystickX = loadAnimationFromSheet(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_JOYSTICK_X), 1, 2, 0.5f, true);
+        Animation joystickY = loadAnimationFromSheet(Assets.instance.getTexture(Assets.BUTTON_CONTROLLER_JOYSTICK_Y), 1, 2, 0.5f, true);
+
+        Animation keyboardE = loadTexture(Assets.instance.getTexture(Assets.BUTTON_KEYBOARD_E));
+        Animation keyboardF = loadTexture(Assets.instance.getTexture(Assets.BUTTON_KEYBOARD_F));
+        Animation keyboardDelete = loadTexture(Assets.instance.getTexture(Assets.BUTTON_KEYBOARD_DELETE));
+        Animation keyboardAD = loadTexture(Assets.instance.getTexture(Assets.BUTTON_KEYBOARD_AD));
+        Animation keyboardWS = loadTexture(Assets.instance.getTexture(Assets.BUTTON_KEYBOARD_WS));
+        Animation padlock = loadTexture(Assets.instance.getTexture(Assets.BUTTON_PADLOCK));
 
         animations = new ArrayList<>();
-        animations.add(faceButtonBase);
-        animations.add(faceButtonhighlightNorth);
-        animations.add(faceButtonHighlightEast);
-        animations.add(faceButtonHighlightSouth);
-        animations.add(faceButtonHighlightWest);
-        animations.add(controllerButtonBack);
-        animations.add(controllerButtonStart);
+        animations.add(faceBlank);
+        animations.add(faceEast);
+        animations.add(faceNorth);
+        animations.add(faceWest);
+        animations.add(faceSouth);
+        animations.add(controllerBack);
+        animations.add(controllerStart);
+        animations.add(joystickX);
+        animations.add(joystickY);
 
-        animations.add(keyBoardEnterButton);
-        animations.add(keyboardBackspaceButton);
-        animations.add(keyboardWButton);
-        animations.add(keyboardSButton);
-        animations.add(keyboardAButton);
-        animations.add(keyboardDButton);
-        animations.add(keyboardEButton);
-        animations.add(keyboardFButton);
-        animations.add(keyboardDeleteButton);
+        animations.add(keyboardE);
+        animations.add(keyboardF);
+        animations.add(keyboardDelete);
+        animations.add(keyboardAD);
+        animations.add(keyboardWS);
         animations.add(padlock);
 
         setAnimation(animationIndex);
@@ -84,7 +77,7 @@ public class ButtonIndicator extends BaseActor {
 
     public void setAnimation(int animation) {
         setAnimation(animations.get(animation));
-        setSize(size ,size);
+        setSize(size, size);
 
     }
 }
