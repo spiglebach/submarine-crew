ac5f192c5af0866281dc032150c6461df8da3d81
Szukács Marcell <spiglebach@gmail.com>
2020-05-24 17:58:50 +0200

    + add Elevator texture - remove unused attributes of Elevator

--- core/src/szm/orde4c/game/entity/submarine/Elevator.java
+++ core/src/szm/orde4c/game/entity/submarine/Elevator.java
@@ -1,17 +1,13 @@
 package szm.orde4c.game.entity.submarine;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import szm.orde4c.game.base.BaseActor;
 import szm.orde4c.game.util.Assets;
 
 public class Elevator extends BaseActor {
-    private String group;
-    private String id;
     public Elevator(float x, float y, float width, float height, Stage s) {
         super(x, y, s);
-        loadTexture(Assets.instance.getTexture(Assets.BLANK)); // TODO textúrát hozzáadni
-        setColor(Color.YELLOW);
+        loadTexture(Assets.instance.getTexture(Assets.SUBMARINE_ELEVATOR));
         setSize(width, height);
         setBoundaryRectangle();
     }
